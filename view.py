81bdf9096983ae176542f50db50e7cf00f5d33c4
Ukolova Nabat <ru.nabat@gmail.com>
2022-08-22 12:19:46 +0400

    first commit

---
+++ view.py
@@ -0,0 +1,26 @@
+# тут функции для работы с файлом csv
+import csv
+import os
+from wsgiref import headers
+
+path = os.path.join('data', 'phone-book.csv')
+def show():
+    with open(path, mode="r", encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=";")
+        print("\nФамилия Имя | Телефон")
+        for row in reader:
+            print(row)#['Surname'], row['Name'],'|', row['Telephone'])
+        
+
+def add_contact():
+    line = []
+    surname = input('Фамилия: ')
+    line.append(surname)
+    name = input('Имя: ')
+    line.append(name)
+    telephone = input('Телефон: ')
+    line.append(telephone)
+    with open(path, mode="a", encoding='utf-8') as csvfile:
+        file_writer = csv.writer(csvfile, delimiter = ";", lineterminator="\r")
+        file_writer.writerow(line)
+    
